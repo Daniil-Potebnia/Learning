fee62455c9bf9242ccdca257a0ee1a42a7dc03d0
Daniil-Potebnia <106385707+Daniil-Potebnia@users.noreply.github.com>
2023-11-11 20:00:36 +0300

    need

--- task_1.py
+++
@@ -1,2 +0,0 @@
-date = '2023-11-11'
-date = {'year': date.split('-')[0], 'month': date.split('-')[1], 'day': date.split('-')[2]}
--- task_4.py
+++
@@ -1,6 +0,0 @@
-def counter(elems, elem):
-    count = 0
-    for n in elems:
-        if n == elem:
-            count += 1
-    return {f'count-{elem}': count}
